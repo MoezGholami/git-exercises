2a4180ceeccafe71241540d61042e7f4b533c3ad
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2017-09-25 16:38:11 -0500

    update big file blunder exercise

--- problems/changing_previous_commits/solution.sh
+++ problems/changing_previous_commits/solution.sh
@@ -2,6 +2,9 @@
 
 #This script must be called inside player's local repository
 
+#pull the commits to see the problem
+git pull origin master
+
 #first find and remove any sql file from all commits. Using an automated/manual way
 git filter-branch --tree-filter 'find . -name "*.sql" | while read path; do rm "$path"; done' HEAD
 git filter-branch --tree-filter 'bash < /dev/tty' HEAD
@@ -9,9 +12,12 @@ git filter-branch --tree-filter 'bash < /dev/tty' HEAD
 #Now it's better to ignore all '.sql' files to prevent such disasters
 echo "# ignore all sql backup files" >> .gitignore
 echo "*.sql" >> .gitignore
+git add .gitignore
+git commit -m "add gitignore to prevent further blunders like this"
+
 
 #Then push all overwritten history to the remote and overrite it
-git push origin master --force --all
+git push --force --all
 
 #You're done with remote and local repository of the player. Now, go to Ali's local repo and
 #overwrite his local repository with the contents of remote
